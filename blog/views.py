18a3a9409c384b2ad6b140b8407a91c3ada24453
Dltmd202 <dltmd202@gmail.com>
2021-01-26 00:17:30 +0900

    Create blog detail page with CBV

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView , DetailView
 
 # Create your views here.
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
