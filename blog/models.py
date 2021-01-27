6b48bc83c5c01c1efc97a9420d359c330ae4e990
Dltmd202 <dltmd202@gmail.com>
2021-01-28 02:37:13 +0900

    Add author Field

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import os
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
@@ -13,8 +14,10 @@ class Post(models.Model):
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/',blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/',blank=True)
 
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
