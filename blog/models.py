fc74fe703d4382bb409f6e8c1dcd1a0e5f6f8998
Dltmd202 <dltmd202@gmail.com>
2021-01-30 02:30:28 +0900

    Create Category page

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_absolute_path(self):
+        return f'/blog/category/{self.slug}'
+
     class Meta:
         verbose_name_plural = 'Categories'
 
