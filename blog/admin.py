6e79120b4a7ac961a178742b6a27e11b79e682b8
Dltmd202 <dltmd202@gmail.com>
2021-02-04 07:26:43 +0900

    Disply comment at post detail page

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 # Register your models here.
 
 
 admin.site.register(Post, MarkdownxModelAdmin)
+admin.site.register(Comment)
+
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -17,6 +19,7 @@ admin.site.register(Category, CategoryAdmin)
 
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+
 
 
 admin.site.register(Tag, TagAdmin)
--- blog/models.py
+++ blog/models.py
@@ -47,7 +47,7 @@ class Post(models.Model):
 
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
 
-    tags = models.ManyToManyField(Tag  , blank=True)
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
@@ -63,3 +63,18 @@ class Post(models.Model):
 
     def get_content_markdown(self):
         return markdown(self.content)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.author}::{self.content}'
+
+    def get_absolute_url(self):
+        return f'{self.post.get_absolute_url()}#comment-{self.pk}'
+
