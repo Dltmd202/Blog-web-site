c51a6b93f2f9c386807c79d3c86bf07a8d27983d
Dltmd202 <dltmd202@gmail.com>
2021-02-06 18:21:44 +0900

    Add comment edit function

--- blog/tests.py
+++ blog/tests.py
@@ -296,7 +296,7 @@ class TestView(TestCase):
         response = self.client.post(
             self.post_001.get_absolute_url() + 'new_comment/',
             {
-                'content':"Kim's comment",
+                'content': "Kim's comment",
             },
             follow=True
         )
@@ -314,3 +314,54 @@ class TestView(TestCase):
         new_comment_div = comment_area.find('div', id=f'comment-{new_comment.pk}')
         self.assertIn('Kim', new_comment_div.text)
         self.assertIn("Kim's comment", new_comment_div.text)
+
+    def test_comment_update(self):
+        comment_by_park = Comment.objects.create(
+            post=self.post_001,
+            author=self.user_Park,
+            content="Park's comment"
+        )
+        response = self.client.get(self.post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        comment_area = soup.find('div', id='comment-area')
+        self.assertFalse(comment_area.find('a', id='comment-1-update-btn'))
+        self.assertFalse(comment_area.find('a', id='comment-2-update-btn'))
+
+        self.client.login(username='kim', password='kimimimi')
+        response = self.client.get(self.post_001.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        comment_area = soup.find('div', id='comment-area')
+        self.assertFalse(comment_area.find('a', id='comment-2-update-btn'))
+        comment_001_update_btn = comment_area.find('a', id='comment-1-update-btn')
+        self.assertIn('edit', comment_001_update_btn.text)
+        self.assertEqual(comment_001_update_btn.attrs['href'], '/blog/update_comment/1/')
+
+        self.assertIn('edit', comment_001_update_btn.text)
+        self.assertEqual(comment_001_update_btn.attrs['href'], '/blog/update_comment/1/')
+
+        response = self.client.get('/blog/update_comment/1/')
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        self.assertEqual('Edit Comment - Blog', soup.title.text)
+        update_comment_form = soup.find('form', id='comment-form')
+        content_textarea = update_comment_form.find('textarea', id='id_content')
+        self.assertIn(self.comment_001.content, content_textarea.text)
+
+        response = self.client.post(
+            f'/blog/update_comment/{self.comment_001.pk}/',
+            {
+                'content': "Edit Park's comment",
+            },
+            follow=True
+        )
+
+        self.assertEqual(response.status_code, 200)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        comment_001_div = soup.find('div', id='comment-1')
+        self.assertIn("Edit Park's comment", comment_001_div.text)
+        self.assertIn('Updated: ',comment_001_div.text)
--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.utils.text import slugify
 from django.shortcuts import get_object_or_404
 from .forms import CommentForm
 
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 
 # Create your views here.
@@ -159,3 +159,14 @@ def new_comment(request, pk):
             return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
+
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
